9c7eaaae710fa74f5ec1c06f35152ff635e77a6d
Yebraksi <ykouzouk@uci.edu>
2017-11-02 22:05:33 -0700

    adding area methode in Dimentions class adding fileds and constractor for Electronics class

--- udemy/src/udemy/Dimensions.java
+++ udemy/src/udemy/Dimensions.java
@@ -4,10 +4,14 @@ public class Dimensions {
 	private double length;
 	private double width;
 	private double hight;
-	public Dimensions (double lenght, double width, double hight){
+	public Dimensions (double length, double width, double hight){
 		this.length=length;
 		this.width= width;
 		this.hight=hight;
+	}
+	private double roomArea(double length, double width, double hight){
+		double area= length*width*hight;
+		return area;
 	}
 
 }
