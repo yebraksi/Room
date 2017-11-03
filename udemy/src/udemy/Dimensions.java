16684389d4043387d29a344e782920553426d0aa
Yebraksi <ykouzouk@uci.edu>
2017-11-02 21:50:47 -0700

    Added fileds and constructor for Dimension class

--- udemy/src/udemy/Dimensions.java
+++ udemy/src/udemy/Dimensions.java
@@ -1,5 +1,13 @@
 package udemy;
 
 public class Dimensions {
+	private double length;
+	private double width;
+	private double hight;
+	public Dimensions (double lenght, double width, double hight){
+		this.length=length;
+		this.width= width;
+		this.hight=hight;
+	}
 
 }
